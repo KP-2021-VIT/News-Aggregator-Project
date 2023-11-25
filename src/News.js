848a0916da9cffe7fe25ede87172540efe7fef98
KP-2021-VIT <150324477+KP-2021-VIT@users.noreply.github.com>
2023-11-25 13:52:55 +0530

    Update News.js

--- src/News.js
+++ src/News.js
@@ -1,68 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import './styles.css';
 const News = () => {
   const [articles, setArticles] = useState([]);
+
   useEffect(() => {
-    const fetchNewsAPI1 = async () => {
+    const fetchNews = async () => {
       try {
-        const apiKey1 = 'bfc79aa9546d455583cb98d54150eec2';
+        const apiKey = 'bfc79aa9546d455583cb98d54150eec2';
         const response = await axios.get(
           `https://newsapi.org/v2/top-headlines?country=in&apiKey=bfc79aa9546d455583cb98d54150eec2`
         );
 
-        setArticles((prevArticles) => [...prevArticles, ...response.data.articles]);
+        setArticles(response.data.articles);
       } catch (error) {
-        console.error('Error fetching news from API 1:', error);
+        console.error('Error fetching news:', error);
       }
     };
 
-    const fetchNewsAPI2 = async () => {
-      try {
-        const apiKey2 = 'pub_334972c0d4353ff94d5ccb232912c833ded53'; 
-        const response = await axios.get(
-          `https://newsdata.io/api/1/news?apikey=pub_334972c0d4353ff94d5ccb232912c833ded53`
-        );
-
-        setArticles((prevArticles) => [...prevArticles, ...response.data.articles]);
-      } catch (error) {
-        console.error('Error fetching news from API 2:', error);
-      }
-    };
-
-    fetchNewsAPI1();
-    fetchNewsAPI2();
-  }, []); 
+    fetchNews();
+  }, []);
 
   return (
-    <div>
-      <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>News Aggregator</h1>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
+    <div className="news-container">
+      <h1 className="news-title">News Aggregator</h1>
+      <ul className="news-list">
         {articles.map((article) => (
-          <li
-            key={article.title}
-            style={{ marginBottom: '30px', borderBottom: '1px solid #ccc', paddingBottom: '20px' }}
-          >
-            <h2 style={{ marginBottom: '15px', fontSize: '1.5rem' }}>{article.title}</h2>
+          <li key={article.title} className="news-item">
+            <h2 className="news-item-title">{article.title}</h2>
             {article.urlToImage && (
               <img
                 src={article.urlToImage}
                 alt={article.title}
-                style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
+                className="news-item-image"
               />
             )}
-            <p style={{ marginTop: '15px', fontSize: '1rem', lineHeight: '1.6' }}>{article.description}</p>
+            <p className="news-item-description">{article.description}</p>
             <a
               href={article.url}
               target="_blank"
               rel="noopener noreferrer"
-              style={{
-                color: '#007bff',
-                textDecoration: 'none',
-                display: 'block',
-                marginTop: '15px',
-                fontWeight: 'bold',
-              }}
+              className="news-item-link"
             >
               Read More
             </a>
